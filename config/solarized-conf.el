4ec598a26d18e3cee151d3c563827de1c7c085f3
Neo Lee <neo.lee@gmail.com>
2017-06-18 22:26:50 -0400

    Change default to dark theme

--- config/solarized-conf.el
+++ config/solarized-conf.el
@@ -3,5 +3,5 @@
 (live-add-pack-lib "solarized")
 (require 'solarized)
 
-(load-theme 'solarized-light t)
-; (load-theme 'solarized-dark t)
+; (load-theme 'solarized-light t)
+(load-theme 'solarized-dark t)
